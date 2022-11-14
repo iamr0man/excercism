7204c6ad91a5a470f339140e030963c44123adf1
iamr0man <koriny4@gmail.com>
2022-11-14 20:53:29 +0200

    add base loop operation;

---
+++ Bird Watcher/script.js
@@ -0,0 +1,48 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+	return birdsPerDay.reduce((acc, curr) => {
+		return acc + curr
+	}, 0)
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+	return totalBirdCount(birdsPerDay.slice((week - 1) * 7, (week * 7)))
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+	if (birdsPerDay.length % 2 !== 0) {
+		return birdsPerDay
+	}
+
+	return birdsPerDay.map((birdCount, index) => {
+		if (index % 2 === 0) {
+			birdsPerDay[index] = birdCount + 1
+		}
+		return birdCount
+	})
+}
