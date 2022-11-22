4583772af2d0b0dd30f701e2a9fb949833b47468
iamr0man <koriny4@gmail.com>
2022-11-22 23:07:15 +0200

    feat: add Coordinate Transform tasks;

---
+++ Coordinate Transform/script.js
@@ -0,0 +1,92 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Create a function that returns a function making use of a closure to
+ * perform a repeatable 2d translation of a coordinate pair.
+ *
+ * @param {number} dx the translate x component
+ * @param {number} dy the translate y component
+ *
+ * @returns {function} a function which takes an x, y parameter, returns the
+ *  translated coordinate pair in the form [x, y]
+ */
+export function translate2d(dx, dy) {
+	return (x = 0, y = 0) => ([dx+x, dy+y])
+}
+
+/**
+ * Create a function that returns a function making use of a closure to
+ * perform a repeatable 2d scale of a coordinate pair.
+ *
+ * @param {number} sx the amount to scale the x component
+ * @param {number} sy the amount to scale the y component
+ *
+ * @returns {function} a function which takes an x, y parameter, returns the
+ *  scaled coordinate pair in the form [x, y]
+ */
+export function scale2d(sx, sy) {
+	return (x = 1, y = 1) => ([sx*x, sy*y])
+}
+
+/**
+ * Create a composition function that returns a function that combines two
+ * functions to perform a repeatable transformation
+ *
+ * @param {function} f the first function to apply
+ * @param {function} g the second function to apply
+ *
+ * @returns {function} a function which takes an x, y parameter, returns the
+ *  transformed coordinate pair in the form [x, y]
+ */
+export function composeTransform(f, g) {
+	return (x, y) => {
+		return g(...f(x, y))
+	}
+}
+
+/**
+ * Return a function that memoizes the last result.  If the arguments are the same as the last call,
+ * then memoized result returned.
+ *
+ * @param {function} f the transformation function to memoize, assumes takes two arguments 'x' and 'y'
+ *
+ * @returns {function} a function which takes x and y arguments, and will either return the saved result
+ *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
+ */
+export function memoizeTransform(f) {
+	let cacheMap = []
+
+	const [memoX, memoY] = f()
+	const params = { memoX, memoY }
+
+	return (x, y) => {
+		const innerParams = { x, y }
+		const updateElement = cacheMap.find(item => {
+			return JSON.stringify({ params: item.params, innerParams: item.innerParams }) === JSON.stringify({ params, innerParams })
+		})
+
+		if (updateElement?.value?.length > 0) {
+			return updateElement.value
+		}
+
+		const value = f(x, y)
+		if (!value) {
+			return [memoX, memoY]
+		}
+
+		const newElement = {
+			params,
+			innerParams,
+			value
+		}
+
+		const newCacheCopy = cacheMap.filter(item => item.params === params && item.innerParams !== innerParams)
+		cacheMap = [...newCacheCopy, newElement]
+
+		return newElement.value
+	}
+}
